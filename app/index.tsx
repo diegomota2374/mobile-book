fbbf533b1cf6c9212d067cb47abc5ccdee0fbbc3
diegomota2374 <diegomota2374@gmail.com>
2024-07-18 10:38:33 -0300

    primeiro commit da configuração do mock de dados e listagem dos elementos

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { TextInput, View } from "react-native";
+
+export default function index (){
+    return(
+        <View>
+            <TextInput placeholder="teste"/>
+        </View>
+    )
+}
